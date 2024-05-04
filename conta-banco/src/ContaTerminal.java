29c2016c86e778f877b4cefffae103e3c947ef8e
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2024-05-03 22:37:18 -0300

    Projeto concluido

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá estou criando meu projeto desafio!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu número de conta :");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite sua agiencia bancaria :");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite seu nome :");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite seu saldo :");
+        double saldo = scanner.nextDouble();
+
+
+        System.out.println("Olá! " + nome + " obrigado por criar uma conta em nosso banco, " + 
+         " sua agência é, " + agencia +  " conta " + numero + " e seu saldo " + saldo + " já está disponível para saque ");
+        
     }
 }
